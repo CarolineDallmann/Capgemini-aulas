9104c0852b01ff1f86585ba6c67db182bcaf807f
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-04-26 09:46:17 -0300

    alterações aula 25-04

--- aula2504/app-agenda/src/app/app-routing.module.ts
+++ aula2504/app-agenda/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroConsultaComponent } from './cadastro-consulta/cadastro-consulta.component';
+import { CadastroContatoComponent } from './cadastro-contato/cadastro-contato.component';
 
 const routes: Routes = [];
 
